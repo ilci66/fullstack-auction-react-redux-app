90f3ad159b4d68aeedc95d324c4f27f112374a4b
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-08-06 15:23:08 +0200

    minor fix

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const User = require('../models/user');
-const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt; 
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
 const options = {
-  jwtFromRequest: ExtractJwt,
+  jwtFromRequest: ExtractJwt.
   fromAuthHeaderAsBearerToken(),
   secretOrKey: PUB_KEY,
   algorithms: ['RS256']
